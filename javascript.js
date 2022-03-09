aaf47840ba7273edf25c1f0ab2058f83914ef908
Jared Hernandez <herjaredsinai@gmail.com>
2022-03-09 17:57:34 -0500

    Create computerPlay function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-console.log("hello");
+
+function computerPlay () {
+    let number = Math.floor(Math.random() *3) +1;
+    
+    switch (number){
+        case 1 :
+            return "rock";
+            break;
+        case 2:
+            return"paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+        default:
+            console.log("something is wrong");
+    }
+}
+
+console.log(computerPlay());
+
