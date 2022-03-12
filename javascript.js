5e8f54a7e353bfd4189509b92d30189017d14902
Jared Hernandez <herjaredsinai@gmail.com>
2022-03-11 19:06:48 -0500

    Remove five round logic

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 
-/** This function make a move for the computer returning either rock,paper,scissors as a string */
+/** This function make a move for the computer returning either
+ * rock,paper,scissors as a string */
 function computerPlay () {
     let number = Math.floor(Math.random() *3) +1;
     
@@ -17,7 +18,8 @@ function computerPlay () {
             console.log("something is wrong");
     }
 }
- /** This function asks player for a move until player inputs either rock paper or scissors */
+ /** This function asks player for a move until player inputs either rock paper
+  * or scissors */
 function playerPlay() {
         let x=0;
         while (x<1){
@@ -36,8 +38,9 @@ function playerPlay() {
             }
         }
 }
-/** This function plays a round of Rock Paper Scissors with the computer and user moves
- * winner is sent to console. 1 is returned if player wins and 0 if player loses. 2 is return if its a tie
+/** This function plays a round of Rock Paper Scissors with the computer and
+ * user moves winner is sent to console. 1 is returned if player wins and 0 if
+ * player loses. 2 is return if its a tie
 */
 function playRound(player,computer){
     if (player === computer) {
@@ -57,22 +60,13 @@ function playRound(player,computer){
         return 0;
     }
 }
-/** This function plays five games of rock paper scissors and displays round results and score and winner at the end */
+/** This function plays five games of rock paper scissors and displays round
+ * results and score and winner at the end */
 function game(){
     let result = 2;
     let playerScore = 0;
     let computerScore =0;
-    for(let i=0;i<5;i++){
-        let playerMove = playerPlay();
-        let computerMove = computerPlay();
-        result = playRound(playerMove,computerMove);
-        if (result === 0) {
-            computerScore++;
-        } else if(result ===1){
-            playerScore++; 
-        }
-        console.log(`Score:   Player ${playerScore} Computer ${computerScore}`);
-    }
+    
     if (playerScore > computerScore){
         console.log(`PLAYER WINS!`) ;
     }else if (playerScore === computerScore){
